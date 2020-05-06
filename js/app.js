4523fc412e51ccfb5f9da3c5ca64ac5680e620e3
Sam Weeks <weeksy_17@icloud.com>
2020-05-06 21:40:23 +0100

    add E-L to lineGraph

--- js/app.js
+++ js/app.js
@@ -1,37 +1,27 @@
-let test1 = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850,2250,1500,2500];
-let test2 = [1850, 1450, 3000, 6000, 9500, 1750, 1250, 1850,2250,1500,2500];
-let test3 = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850,2250,1500,2500];
-let test4 = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850,2250,1500,2500];
+let hourArr = [75, 125, 16, 200, 150, 127, 25, 183,778,130,117];
+let dailyArr = [850, 1450, 1326, 1117, 2550, 252, 1250, 1850,2250,1500,2500];
+let weeklyArr = [1950, 6250, 3000, 5070, 4500, 1750, 1250, 1850,2650,1500,2500];
+let monthlyArr = [12750, 41280, 37000, 26800, 71500, 51750, 21250, 41850,21250,25500,22500];
 
-const trafficData =  {
-    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
-    datasets: [{
-        label: 'Traffic',
-        backgroundColor: 'rgb(77,76,114,.8)',
-        borderColor: 'rgb(116,119,191)',
-        data: test4,
-        borderWidth: 1,
-    }]
-}
-const trafficData2 =  {
-    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
-    datasets: [{
-        label: 'Traffic',
-        backgroundColor: 'rgb(77,76,114,.8)',
-        borderColor: 'rgb(116,119,191)',
-        data: test2,
-        borderWidth: 1,
-    }]
-}
 
-// ! Line chart top of page
+
+// ! Line chart top of page's initial value
 const lineChart = document.getElementById('lineChart').getContext('2d');
-const lChart = new Chart(lineChart, {
+const start = new Chart(lineChart, {
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
-    data: trafficData,
+    data:  {
+         labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+    datasets: [{
+        label: 'Traffic',
+        backgroundColor: 'rgb(77,76,114,.8)',
+        borderColor: 'rgb(116,119,191)',
+        data: hourArr,
+        borderWidth: 1,
+    }]
+},
 
     // Configuration options go here
     options: {
@@ -48,15 +38,151 @@ const lChart = new Chart(lineChart, {
         }
     }
 });
-
+// ! Switch between line graph data 
 const dashLineBtns = document.querySelector('.dash-line-btns');
 dashLineBtns.addEventListener('click', (e) => {
-    const hourly = document.querySelector('#hourly');
-    const daily = document.querySelector('#daily');
-    const weekly = document.querySelector('#weekly');
-    const monthly = document.querySelector('#monthly');
-    if (e.target === hourly ) return lChart.data = trafficData2;
+    const hourlyLi = document.querySelector('#hourly');
+    const dailyLi = document.querySelector('#daily');
+    const weeklyLi= document.querySelector('#weekly');
+    const monthlyLi= document.querySelector('#monthly');
+    if (e.target === hourlyLi ) {
+        const hourly = new Chart(lineChart, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data:  {
+                 labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+            datasets: [{
+                label: 'Traffic',
+                backgroundColor: 'rgb(77,76,114,.8)',
+                borderColor: 'rgb(116,119,191)',
+                data: test1,
+                borderWidth: 1,
+            }]
+        },
+        
+            // Configuration options go here
+            options: {
+                legend: {
+                   display: false
+                },
+                events: ['click'],
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    if (e.target === dailyLi ) {
+        const daily = new Chart(lineChart, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data:  {
+                labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+           datasets: [{
+               label: 'Traffic',
+               backgroundColor: 'rgb(77,76,114,.8)',
+               borderColor: 'rgb(116,119,191)',
+               data: dailyArr,
+               borderWidth: 1,
+           }]
+        },
+        
+            // Configuration options go here
+            options: {
+                legend: {
+                   display: false
+                },
+                events: ['click'],
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    if (e.target === weeklyLi ){
+        const weekly = new Chart(lineChart, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data:  {
+                labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+           datasets: [{
+               label: 'Traffic',
+               backgroundColor: 'rgb(77,76,114,.8)',
+               borderColor: 'rgb(116,119,191)',
+               data: weeklyArr,
+               borderWidth: 1,
+           }]
+        },
+        
+            // Configuration options go here
+            options: {
+                legend: {
+                   display: false
+                },
+                events: ['click'],
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    if (e.target === monthlyLi ) {
+        const monthly = new Chart(lineChart, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data:  {
+                labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3','4-10','11-17','18-24','25-31'],
+           datasets: [{
+               label: 'Traffic',
+               backgroundColor: 'rgb(77,76,114,.8)',
+               borderColor: 'rgb(116,119,191)',
+               data: monthlyArr,
+               borderWidth: 1,
+           }]
+        },
+        
+            // Configuration options go here
+            options: {
+                legend: {
+                   display: false
+                },
+                events: ['click'],
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 }); 
+
+
+
+
+
 
 // ! barChart here 
 const barChart = document.getElementById('barChart').getContext('2d');
@@ -78,6 +204,15 @@ const bChart = new Chart(barChart, {
 
     // Configuration options go here
     options: {
+        legend: {
+            position: 'top',
+            align: 'start',
+            labels: {
+                boxWidth: 15,
+                padding: 20
+                
+            }
+        },
         scales: {
             yAxes: [{
                 ticks: {
@@ -135,3 +270,16 @@ const dChart = new Chart(doughnutChart, {
          }
     }
 });
+
+// ! the code for the alert to go here
+const alertDiv = document.querySelector('.alert-message');
+
+const alertMessage = () => {
+    const alertDiv = document.querySelector('.alert-message');
+    const alert = document.createElement('p');
+    alert.innerHTML = `this is a test informing myself about the test and its testiness`
+    alertDiv.appendChild(alert);
+}
+alertMessage();
+const bell = document.querySelector('.icon-bell');
+bell.addEventListener('click', () =>  alertDiv.classList.toggle('alert'));
